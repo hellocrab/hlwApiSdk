96b50d80a91d9cdcbc3cf3b76cc548d94b9c7433
yanghao <389812249@qq.com>
2018-12-20 09:42:20 +0800

    @

--- define/huiliewang/dataobject/job/job.thrift
+++ define/huiliewang/dataobject/job/job.thrift
@@ -4,10 +4,11 @@ namespace * com.hlw.huiliewang.dataobject.job
 struct JobResumesRequestDTO 
 {
 	1:i32 job_id,#慧猎网前台职位ID
-	2: optional map<string,string> resume,
-	3: optional map<string,string> resume_data,
-	4: optional map<i32,map<string,string>> resume_edu,
-	5: optional map<i32,map<string,string>> resume_languages,
-	6: optional map<i32,map<string,string>> resume_project,
-	7: optional map<i32,map<string,string>> resume_work,
+        2:i32 boss_resume_id,
+	3: optional map<string,string> resume,
+	4: optional map<string,string> resume_data,
+	5: optional map<i32,map<string,string>> resume_edu,
+	6: optional map<i32,map<string,string>> resume_languages,
+	7: optional map<i32,map<string,string>> resume_project,
+	8: optional map<i32,map<string,string>> resume_work,
 }
